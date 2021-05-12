e05f336189648179e37f68fea1dbed79c2a33f70
monsonjeremy <monson.jeremy@gmail.com>
2021-05-11 22:50:35 -0700

    remove unused code from lsp

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -63,19 +63,3 @@ lspconfig.jsonls.setup {
   on_attach=on_attach,
   cmd = {"json-languageserver", "--stdio"}
 }
-
-local function eslint_config_exists()
-  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
-
-  if not vim.tbl_isempty(eslintrc) then
-    return true
-  end
-
-  if vim.fn.filereadable("package.json") then
-    if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
-      return true
-    end
-  end
-
-  return false
-end
