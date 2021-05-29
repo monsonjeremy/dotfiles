1447bb72cbee1955b3471a62ca99bdf0544e86ce
monsonjeremy <monson.jeremy@gmail.com>
2021-05-29 14:53:37 -0700

    point to asdf python

--- config/nvim/lua/config/init.lua
+++ config/nvim/lua/config/init.lua
@@ -2,5 +2,5 @@ require('config.options')
 require('config.mappings')
 require('config.functions')
 
-vim.g["python_host_prog"] = '~/.asdf/installs/2.7.18/bin/python'
-vim.g["python3_host_prog"] = '~/.asdf/installs/3.9.5/bin/python'
+vim.g["python_host_prog"] = '~/.asdf/installs/python/2.7.18/bin/python'
+vim.g["python3_host_prog"] = '~/.asdf/installs/python/3.9.5/bin/python'
