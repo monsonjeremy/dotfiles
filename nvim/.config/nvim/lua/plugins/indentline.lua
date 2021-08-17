2f44dbc4a83f68849fb9ece3001a87a1a01ddc14
monsonjeremy <monson.jeremy@gmail.com>
2021-08-16 22:50:36 -0700

    fix dd key not working properly

--- nvim/.config/nvim/lua/plugins/indentline.lua
+++ nvim/.config/nvim/lua/plugins/indentline.lua
@@ -5,8 +5,8 @@ indentBlankline.setup({
   -- char = '│',
   char = '▏',
   show_first_indent_level = false,
-  filetype_exclude = { 'help' },
-  buftype_exclude = { 'terminal', 'WhichKey', 'dashboard', 'help' },
+  filetype_exclude = { 'help', 'packer' },
+  buftype_exclude = { 'terminal', 'WhichKey', 'dashboard', 'help', 'packer' },
   show_current_context = true,
   show_trailing_blankline_indent = false,
 })
