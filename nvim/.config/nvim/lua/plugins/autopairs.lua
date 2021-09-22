c7ae85c6d552e9cafd6ecb20587ebaa2d94163e9
monsonjeremy <monson.jeremy@gmail.com>
2021-09-21 20:21:48 -0700

    fix enter

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,10 @@
-local present, autopairs = pcall(require, 'nvim-autopairs')
+local present1, autopairs = pcall(require, 'nvim-autopairs')
+local present2, autopairs_completion = pcall(require, 'nvim-autopairs.completion.cmp')
 
-if not present then return end
+if not (present1 or present2) then return end
 
 autopairs.setup({ check_ts = true })
-
+autopairs_completion.setup({
+  map_cr = true, --  map <CR> on insert mode
+  map_complete = true, -- it will auto insert `(` after select function or method item
+})
