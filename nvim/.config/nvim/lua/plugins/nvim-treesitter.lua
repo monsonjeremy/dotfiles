63e12ed718a1b271879bb5e47a0865815234fbf0
monsonjeremy <monson.jeremy@gmail.com>
2021-11-24 20:36:41 -0800

    treesitter inc selection

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -10,4 +10,13 @@ treesitter.setup({
   ensure_installed = 'maintained',
   highlight = { enable = true, use_languagetree = true },
   refactor = { highlight_definitions = { enable = true }, navigation = { enable = true } },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      scope_incremental = '<CR>',
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
+    },
+  },
 })
